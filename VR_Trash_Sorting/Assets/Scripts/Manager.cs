7e9990496293cb95f4d76bceff3b9fc03f48f3f0
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2019-11-05 11:59:26 +0100

    Had to reset the project due to VR malfunction

---
+++ VR_Trash_Sorting/Assets/Scripts/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private string _username;
+    private bool _pamphletUser;
+    private int _score;
+
+    //Set using UI input
+    public void SetUsername(string username)
+    {
+        _username = username;
+    }
+
+    //Set using UI input
+    public void SetUserType(bool pamphletUser)
+    {
+        _pamphletUser = pamphletUser;
+    }
+
+    public void DeductPoints(int pointsToDeduct)
+    {
+        _score -= pointsToDeduct;
+    }
+
+    public void AddPoints(int pointsToAdd)
+    {
+        _score += pointsToAdd;
+    }
+
+    //Called from game over button press
+    public void SaveToFile()
+    {
+        
+    }
+}
